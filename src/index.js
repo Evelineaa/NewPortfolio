3088f772adb2b9f81afb2f02fe99910d69d41d12
Milla Pohjolainen <millapohj@gmail.com>
2025-08-01 15:58:39 +0300

    Mainsite ready and projects main almost

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import ProjectsMenu from './ProjectsMenu';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/NewPortfolio">
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Projects" element={<Projects />} />
